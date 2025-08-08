6752628e931c08e8ae231be55c9de747f918e160
IMuhim <muhim.istiak@gmail.com>
2025-08-08 15:17:21 +0100

    added function to check whether player has won or game is tied

--- game.js
+++ game.js
@@ -29,6 +29,25 @@ const winCombinations = [ // Winning combinations for the game
     [1, 5, 9], [3, 5, 7] // Diagonal
 ]
 
+function winCheck(player) { // Check if the player has won
+    for (let i=0; i < winCombinations.length; i++) {
+        if (grid[winCombinations[i][0]] === player &&
+            grid[winCombinations[i][1]] === player &&
+            grid[winCombinations[i][2]] === player) {
+            return true
+        }
+    }
+    return false
+}
+
+function tieCheck() { // A tie occurs when all spaces are filled and no player has won
+    for (let i = 1; i <= 9; i++) {
+        if (grid[i] === '') {
+            return false
+        }
+    }
+    return true
+}
 
 console.log('Game started: \n' + // Instructions for the game
     'Select a number between 1 and 9 to place your mark. \n' +
